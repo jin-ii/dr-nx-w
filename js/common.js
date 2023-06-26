136195437c7fb0bd0434fdcff488740c68620a7c
J <Y>
2023-06-26 16:50:55 +0900

    페이지추가(로그인,사이트맵 등),서브레이아웃 반응형 수정

--- js/common.js
+++ js/common.js
@@ -2,23 +2,23 @@ $(document).ready(function () {
 
 	// layout mobile menu -- 추후 삭제
 	fetch('./mobile-menu.html')
-	.then(res => res.text())
-	.then(function(data) {
-		document.getElementById("mobile-menu").innerHTML = data;
-	})
+		.then(res => res.text())
+		.then(function (data) {
+			document.getElementById("mobile-menu").innerHTML = data;
+		});
 
 
-	
-	
+
+
 	var btnmenu = 0;
-	$(".btnmenu").click(function(e){
-			console.log('hi');
-		if($(window).width() > 900) {
+	$(".btnmenu").click(function (e) {
+		console.log('hi');
+		if ($(window).width() > 900) {
 			// Redirect to another link
 			window.location.href = "sitemap.html";
 			return;
 		}
-		
+
 		if (btnmenu == 0) {
 			$(this).stop().addClass("openmenu");
 			$(".allmenu").addClass("open");
@@ -28,8 +28,8 @@ $(document).ready(function () {
 			$(".allmenu").removeClass("open");
 			btnmenu = 0;
 		}
-		
-		e.preventDefault(); // Prevent default link behavior
+
+		e.preventDefault(); // Prevent default link behavior` 
 	});
 
 	$(" .pc_nav>ul>li").mouseover(function () {
@@ -48,31 +48,39 @@ $(document).ready(function () {
 	// aos
 	AOS.init();
 
-	
+
 
 
 	//모바일메뉴
 	$(".mobile_sub").hide()
 
 	// nav menu
-	$(".list-item").click(function(e) {
+	$(".list-item").click(function (e) {
 		e.preventDefault();
 		$(".mobile_sub").slideUp().removeClass("on")
 		$(this).toggleClass("on").next().stop().slideToggle()
-
-		
 	});
 
-	$(".sub_tab_list li a").click(function(e) {
-		e.preventDefault();
-		$(".sub-depth-2").slideUp().removeClass("on")
-		$(this).toggleClass("on").next().stop().slideToggle()
-
-		
-	})
 
 
+	// 모바일 일때 서브 메뉴 숨기기
+	function checkViewportWidth() {
+		if (window.innerWidth > 690) $(".sub-depth-2").hide()
+		// else $(".sub_tab_list li a").addClass("on")
+	}
 
-	
+	checkViewportWidth();
+
+	window.addEventListener("resize", checkViewportWidth);
+
+	$(".sub_tab_list li a").click(function (e) {
+		// sub menu child show toggle - with mobile
+		if (window.innerWidth <= 690) {
+			e.preventDefault();
+			$(".sub-depth-2").slideToggle()
+			
+		}
+	});
+
 
 });
